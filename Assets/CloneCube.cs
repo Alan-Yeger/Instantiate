e4c932f6bba1c72519ed21af6fd70340529d610a
Alan-Yeger <alanyeger1@gmail.com>
2022-05-04 15:50:12 -0300

    Initial commit

---
+++ Assets/CloneCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneCube : MonoBehaviour
+{
+    public GameObject ClonarCubito;
+
+    public void CloneObject()
+    {
+        int i = 0;
+        while (i < 3)
+        {
+            Instantiate(ClonarCubito);
+            i++;
+        }
+            
+    }
+}
